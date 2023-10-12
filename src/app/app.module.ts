b19dfcc990092573aa3ff3613ba7f8ed9608255f
Pedro Reis <itspdro@outlook.com>
2023-10-11 21:30:59 -0300

    chore: updated card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardDataComponent } from './components/card/card-data/card-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    NavBarComponent
+    NavBarComponent,
+    CardLabelComponent,
+    CardDataComponent
   ],
   imports: [
     BrowserModule,
